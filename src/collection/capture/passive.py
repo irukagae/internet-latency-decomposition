ac33fd289bd747d53cf5063131a5b10a246f4f13
Vedang-K <vedangkulk26@gmail.com>
2026-02-01 19:34:49 +0530

    Add passive packet capture bounded by experiment time window

---
+++ src/collection/capture/passive.py
@@ -0,0 +1,23 @@
+import time
+from scapy.all import sniff
+
+def capture_packets(interface: str, start_time: float, end_time: float):
+    """Passively captures packet on a given interface within a time window"""
+
+    if end_time <= start_time:
+        raise ValueError("end_time must be greater than start_time.")
+
+    captured_packets = []
+
+    # wait until capture window opens
+    now = time.time()
+    if now < start_time:
+        time.sleep(start_time - now)
+
+    def packet_handler(pkt):
+        captured_packets.append(pkt)
+
+    # start sniffing until end_time is reached
+    sniff(iface=interface,prn=packet_handler, store=False, stop_filter=lambda _: time.time() >= end_time)
+
+    return captured_packets

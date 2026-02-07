0b629e92caa02b6da3c6f801f7ea4ed4e1b1f2a6
Vedang-K <vedangkulk26@gmail.com>
2026-02-07 21:30:43 +0530

    Update network interface and targets

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -1,11 +1,30 @@
 import os
 import time
 import uuid
-from datetime import datetime
+from datetime import datetime, UTC
+from scapy.all import get_if_list, get_if_addr
+from pathlib import Path
 
 from collection.experiment.runner import run_expt
 from collection.capture.passive import capture_packets
 from collection.persistence.csv_writer import write_passive_packets, write_active_probe
+
+project_root = Path(__file__).resolve().parent.parent
+data_dir = project_root / "data" / "raw"
+
+def get_active_interface():
+    """Automatically identifies the currently active network interface."""
+
+    for iface in get_if_list():
+        try:
+            ip = get_if_addr(iface)
+            if ip.startswith("10.") or ip.startswith("192.") or ip.startswith("172."):
+                return iface
+
+        except Exception:
+            continue
+
+    return None
 
 def connect_vpn(location_code: str, warmup_time = 15):
     """Connects Mullvad VPN to the desired location."""
@@ -34,24 +53,23 @@ num_probes = 30
 probe_interval = 0.25
 timeout = 1.0
 
-network_interface = "wlan0"
-
 def run_single_expt(protocol, packet_size, dst_ip, source_location, vpn_provider):
     expt_id = uuid.uuid4().hex
-    timestamp = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+    timestamp = datetime.now(UTC).strftime('%Y-%m-%dT%H:%M:%S')
 
-    output_dir = os.path.join("data", "raw")
-    os.makedirs(output_dir, exist_ok=True)
+    data_dir.mkdir(parents=True, exist_ok=True)
 
     print(f"[EXPT] ID = {expt_id}")
     print(f"[EXPT] Protocol={protocol}, Size={packet_size}, Src={source_location}")
+
+    active_iface = get_active_interface()
 
     capture_start = time.time()
     estimated_duration = (num_probes * probe_interval + timeout + 2.0)
     capture_end = capture_start + estimated_duration
 
     # passive capture
-    packets = capture_packets(interface=network_interface, start_time=capture_start, end_time=capture_end)
+    packets = capture_packets(interface=active_iface, start_time=capture_start, end_time=capture_end)
 
     # active probing
     results = run_expt(protocol=protocol, dst_ip=dst_ip, packet_size=packet_size,
@@ -63,15 +81,15 @@ def run_single_expt(protocol, packet_size, dst_ip, source_location, vpn_provider
         row["experiment_id"] = expt_id
         row["timestamp"] = timestamp
 
-    write_active_probe(results, os.path.join(output_dir, f"active_probe_raw_{datetime.now().strftime('%Y_%M')}.csv"))
-    write_passive_packets(packets, os.path.join(output_dir, f"passive_probe_raw_{datetime.now().strftime('%Y_%M')}.csv"),
+    write_active_probe(results, str(data_dir / f"active_probe_raw_{datetime.now().strftime('%Y_%m')}.csv"))
+    write_passive_packets(packets, str(data_dir / f"passive_probe_raw_{datetime.now().strftime('%Y_%m')}.csv"),
                           source_location=source_location, vpn_provider=vpn_provider, experiment_id=expt_id,
                           experiment_timestamp=timestamp)
 
-    print(f"[EXPT] Completed -> {output_dir}\n")
+    print(f"[EXPT] Completed -> {data_dir}\n")
 
 def main():
-    target = "8.8.8.8"
+    target = ["8.8.8.8", "1.1.1.1", "9.9.9.9"]
 
     print("STARTING DATA COLLECTION...\n")
 
@@ -83,18 +101,16 @@ def main():
             connect_vpn(loc)
             vpn_provider = "Mullvad"
 
-        for prt in protocol:
-            for pktsize in packet_size:
-                run_single_expt(protocol=prt, packet_size=pktsize,
-                                dst_ip=target, source_location=loc,
-                                vpn_provider=vpn_provider)
+        for tar in target:
+            for prt in protocol:
+                for pktsize in packet_size:
+                    run_single_expt(protocol=prt, packet_size=pktsize,
+                                    dst_ip=tar, source_location=loc,
+                                    vpn_provider=vpn_provider)
 
     disconnect_vpn()
 
-
-
     print("ALL EXPERIMENT COMPLETED.")
-
 
 if __name__ == '__main__':
     main()

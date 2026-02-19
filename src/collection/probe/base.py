f8935292982d9d90b15d2743a68f7db8a948c7af
Vedang-K <vedangkulk26@gmail.com>
2026-02-19 20:33:35 +0530

    Update measure_rtt to accept network interface

--- src/collection/probe/base.py
+++ src/collection/probe/base.py
@@ -2,12 +2,12 @@ import time
 from scapy.all import sr1
 
 
-def measure_rtt(packet, timeout=1.0):
+def measure_rtt(packet, timeout=1.0, iface=None):
     """Measures round-trip time (RTT) for a given packet"""
 
     send_timestamp = time.monotonic()  # record timestamp immediately before sending the packet
 
-    response = sr1(packet, timeout=timeout, verbose=0)  # send one packet and wait for a single response
+    response = sr1(packet, timeout=timeout, verbose=0, iface=iface)  # send one packet and wait for a single response
 
     if response is None:  # if no response is received
         return {"success": False,

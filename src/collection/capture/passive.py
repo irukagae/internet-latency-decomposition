937f27aa27a3db3c28e0d30575f4e337e712be1a
Vedang-K <vedangkulk26@gmail.com>
2026-04-01 00:28:14 +0530

    fix: add sniff() timeout to prevent idle network hang

--- src/collection/capture/passive.py
+++ src/collection/capture/passive.py
@@ -17,7 +17,9 @@ def capture_packets(interface: str, start_time: float, end_time: float):
     def packet_handler(pkt):
         captured_packets.append(pkt)
 
+    duration = end_time - time.time()
     # start sniffing until end_time is reached
-    sniff(iface=interface,prn=packet_handler, store=False, stop_filter=lambda _: time.time() >= end_time)
+    sniff(iface=interface,prn=packet_handler, store=False,
+          stop_filter=lambda _: time.time() >= end_time, timeout=duration)
 
     return captured_packets
